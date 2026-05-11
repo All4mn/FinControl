020354686e08a20ecc896c8b460c492aa807a2ab
Dedei-AB <andreiabners@gmail.com>
2026-05-11 16:17:35 -0300

    Dependências atualizadas

--- FC_Backend/src/app.js
+++ FC_Backend/src/app.js
@@ -3,7 +3,7 @@
 // =============================================================================
 
 import fastify from "fastify";
-import cors from '@fastify/cors';
+import cors from "@fastify/cors";
 import dotenv from "dotenv";
 
 // Importando as rotas do Fastify
@@ -19,31 +19,32 @@ dotenv.config();
 const app = fastify({ logger: true });
 
 app.register(cors, {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 });
 
 // Registra as rotas com seus prefixos.
 // Ex: Tudo que estiver em usuarioRoutes será acessado via /usuarios
-app.register(usuarioRoutes, { prefix: '/usuarios' });
-app.register(categoriaRoutes, { prefix: '/categorias' });
-app.register(metodoRoutes, { prefix: '/metodos' });
-app.register(contaRoutes, { prefix: '/contas' });
-app.register(transacaoRoutes, { prefix: '/transacoes' });
-app.register(logsRoutes, { prefix: '/logs' });
+app.register(usuarioRoutes, { prefix: "/usuarios" });
+app.register(categoriaRoutes, { prefix: "/categorias" });
+app.register(metodoRoutes, { prefix: "/metodos" });
+app.register(contaRoutes, { prefix: "/contas" });
+app.register(transacaoRoutes, { prefix: "/transacoes" });
+app.register(logsRoutes, { prefix: "/logs" });
 
 const start = async () => {
-    try {
-        await app.listen({ port: 3000 });
-        console.log("Servidor Fastify rodando na porta 3000");
-        console.log("Conectando ao banco de dados...");
-        // console.log(process.env.DB_CONNECTION_STRING); // <-- Adicione esta linha para verificar a string de conexão se der undefined então falta configurar a variável de ambiente
-    } catch (err) {
-        app.log.error(err);
-        process.exit(1);
-    }
+  try {
+    console.log("Conectando ao banco de dados...");
+    await app.listen({ port: process.env.PORT || 3000 });
+    console.log("Servidor conectado ao banco de dados com sucesso!");
+    console.log("Servidor Fastify rodando na porta:");
+    // console.log(process.env.DB_CONNECTION_STRING); // <-- Adicione esta linha para verificar a string de conexão se der undefined então falta configurar a variável de ambiente
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
 };
 
 start();
