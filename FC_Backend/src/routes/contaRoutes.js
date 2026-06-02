c5f67a0f252f2d3befee73f1f49af4113257d894
Allan <153043227+All4mn@users.noreply.github.com>
2026-06-02 20:08:10 -0300

    Deletei o FC_Backend antigo

--- FC_Backend/src/routes/contaRoutes.js
+++
@@ -1,9 +0,0 @@
-import ContaController from '../controllers/contaController.js';
-
-export async function contaRoutes(app) {
-    app.get('/', ContaController.listar);
-    app.get('/:id', ContaController.buscarPorId);
-    app.post('/', ContaController.criar);
-    app.put('/:id', ContaController.atualizar);
-    app.delete('/:id', ContaController.deletar);
-}
--- FC_Backend/src/routes/logsRoutes.js
+++
@@ -1,5 +0,0 @@
-import logsController from "../controllers/logsController.js";
-
-export async function logsRoutes(app) {
-    app.get('/', logsController.listar);
-}
